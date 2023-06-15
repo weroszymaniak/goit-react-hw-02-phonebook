bff70b8dc3433a8a42b4bcd28652d335dfbe8117
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-06-15 20:44:30 +0200

    changes in phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = contact => {
+    const isInContacts = this.state.contacts.some(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isInContacts) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
+    }));
+  };
+  removeContact = () => console.log('removin');
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <ContactForm onSubmit={this.addContact}></ContactForm>
+        <h2>Contact List</h2>
+        <ContactList
+          contacts={contacts}
+          removeContact={this.removeContact}
+        ></ContactList>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+
+const ContactList = ({ contacts, removeContact }) => (
+  <ul>
+    {contacts.map(contact => (
+      <li key={contact.id}>
+        {contact.name + ' : ' + contact.number}
+        {
+          <button
+            type="button"
+            name="delete"
+            onClick={() => removeContact(contact.id)}
+          >
+            delete
+          </button>
+        }
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.object.isRequired,
+  removeContact: Proptypes.func.isRequired,
+};
+
+export default ContactList;
